0a223625a84f11b9ea23628851e960172e53e9fd
0abdullah0sami0 <0abdullah0sami0@gmail.com>
2024-04-18 13:44:45 +0300

    Add Social media links

--- ui.R
+++ ui.R
@@ -220,13 +220,13 @@ dashboardPage(
     ),
     
     hr(),
-    # fluidRow(
-    #   column(width = 1,align = "right", offset = 4,
-    #          tags$a(img(src = "GitHub.png",style = "width:30%;height:30%;"),href = "https://github.com/0abdullah0sami0", target="_blank")),
-    #   column(width = 1,align = "center", offset = 0,
-    #          tags$a(img(src = "LinkedIn.png",style = "width:30%;height:30%;"),href = "https://www.linkedin.com/in/abdullahalshalaan/", target="_blank")),
-    #   column(width = 1,align = "left", offset = 0,
-    #          tags$a(img(src = "twittter.jpeg",style = "width:30%;height:30%;"),href = "https://twitter.com/HR02030", target="_blank"))
-    # )
+    fluidRow(
+      column(width = 1,align = "right", offset = 4,
+             tags$a(img(src = "GitHub.png",style = "width:30%;height:30%;"),href = "https://github.com/0abdullah0sami0", target="_blank")),
+      column(width = 1,align = "center", offset = 0,
+             tags$a(img(src = "LinkedIn.png",style = "width:30%;height:30%;"),href = "https://www.linkedin.com/in/abdullahalshalaan/", target="_blank")),
+      column(width = 1,align = "left", offset = 0,
+             tags$a(img(src = "twittter.jpeg",style = "width:30%;height:30%;"),href = "https://twitter.com/HR02030", target="_blank"))
+    )
   )
 )
